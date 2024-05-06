c1c77027219f91d51dc4eb77a7cc3732f7cb2a32
Robson <robsonmoura970@hotmail.com>
2024-05-06 10:28:32 -0300

    first commit

---
+++ consultacarros/src/main/java/br/com/consultacarros/ConsultacarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.consultacarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultacarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultacarrosApplication.class, args);
+	}
+
+}
---
+++ consultacarros/src/test/java/br/com/consultacarros/ConsultacarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultacarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultacarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
